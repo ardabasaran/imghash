0a5cee78a1ff0efc00811d699adbfb2a07405596
Arda Basaran <ardarbasaran@gmail.com>
2019-03-12 02:26:00 -0400

    Added PHash implementation

--- distance.go
+++ distance.go
@@ -1,7 +1,7 @@
 package imghash
 
-func HammingDistance(d1, d2 uint64) int64 {
-	distance := int64(0)
+func HammingDistance(d1, d2 uint64) uint64 {
+	distance := uint64(0)
 	for bitPos := uint(0); bitPos < 64; bitPos++ {
 		d1Bit := d1 & (1 << bitPos)
 		d2Bit := d2 & (1 << bitPos)
---
+++ imghash_test.go
@@ -0,0 +1,86 @@
+package imghash
+
+import (
+	"fmt"
+	"github.com/nfnt/resize"
+	"image"
+	_ "image/jpeg"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+type ImageNamePair struct{
+	img image.Image
+	filename string
+}
+
+var tests []ImageNamePair
+
+
+func setup() {
+	files, err := ioutil.ReadDir("./resources")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if file.Name()[0] == '.' {
+			continue
+		}
+		fmt.Printf("Reading file %v\n", file.Name())
+		infile, err := os.Open("resources/" + file.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+		src, _, err := image.Decode(infile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tests = append(tests, ImageNamePair{img:src, filename:file.Name()})
+
+		err = infile.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	os.Exit(m.Run())
+}
+
+func TestAhash(t *testing.T) {
+	for _, imageNamePair := range tests {
+		hashValue := Ahash(&imageNamePair.img)
+		t.Logf("Filename: %v\t Ahash: %016x",imageNamePair.filename, hashValue)
+	}
+}
+
+func TestPhash(t *testing.T) {
+	for _, imageNamePair := range tests {
+		hashValue := Phash(&imageNamePair.img)
+		t.Logf("Filename: %v\t Ahash: %016x",imageNamePair.filename, hashValue)
+	}
+}
+
+func BenchmarkResize(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		resize.Resize(8, 8, tests[0].img, INTERP)
+	}
+}
+
+func BenchmarkAhash(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Ahash(&tests[0].img)
+	}
+}
+
+func BenchmarkPhash(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Ahash(&tests[0].img)
+	}
+}
